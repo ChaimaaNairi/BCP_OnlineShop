5051558a917d8f72f82a61d97787d1fe39434a1e
Chaimaa Nairi <83820363+ChaimaaNairi@users.noreply.github.com>
2023-03-03 22:15:54 +0300

    Update firebase.js

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEF3jeZp4ui526w3cc21TqP8lX9p8pT4E",
-  authDomain: "bc-shop-82b02.firebaseapp.com",
-  projectId: "bc-shop-82b02",
-  storageBucket: "bc-shop-82b02.appspot.com",
-  messagingSenderId: "931820278874",
-  appId: "1:931820278874:web:cb40103243b93f93cf3463"
+  apiKey: "*******",
+  authDomain: "******",
+  projectId: "*****",
+  storageBucket: "***",
+  messagingSenderId: "***",
+  appId: "*****"
 };
 
 // Initialize Firebase
